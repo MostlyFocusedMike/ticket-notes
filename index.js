a72c3e1442252f09cbd124400fa328a4d9dc8257
Mike <mostlyfocusedmike@gmail.com>
2018-10-19 15:57:22 -0400

    add console checks with the tests

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ function makeEntry() {
   const date = new Date();
   const pullRequestStr = process.argv[2];
   const files = process.argv.slice(3);
-  const PARENT_DIR = files[0].match(process.env.PARENT_DIR + "/([^\/]+)/")[1];
+  const REPO = files[0].match(process.env.PARENT_DIR + "/([^\/]+)/")[1];
 
-  const pullRequestTicket = pullRequestStr.match(/^\[(.+)\]/)[1];
-  const pullRequestName = pullRequestStr.match(/\] (.+)\#/)[1];
-  const pullRequestNumber = pullRequestStr.match(/\#(\d+)/)[1];
+  const pullRequestTicket = pullRequestStr.match(/^\[(.+)\]/)[1].trim();
+  const pullRequestName = pullRequestStr.match(/\] (.+)\#/)[1].trim();
+  const pullRequestNumber = pullRequestStr.match(/\#(\d+)/)[1].trim();
 
   console.log(`
 ${pullRequestTicket} https://${process.env.BOARD}/${pullRequestTicket}
@@ -38,9 +38,9 @@ Date: ${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear() - 2000}
 Description:
 Fix:
 Files:
-\t${PARENT_DIR.toUpperCase()}
-\t${files.map(file => file.split(PARENT_DIR)[1]).join("\n\t")}
-Commits: https://github.com/${process.env.GITHUB_PROFILE}/${PARENT_DIR}/pull/${pullRequestNumber}/commits
+\t${REPO.toUpperCase()}
+\t${files.map(file => file.split(REPO)[1]).join("\n\t")}
+Commits: https://github.com/${process.env.GITHUB_PROFILE}/${REPO}/pull/${pullRequestNumber}/commits
 `)
   return true; // since the log will not return anything
 }
--- index.test.js
+++ index.test.js
@@ -5,6 +5,9 @@ describe('Main test', () => {
     process.env.BOARD = "kanban.com"
     process.env.GITHUB_PROFILE = "company-name"
     process.env.PARENT_DIR = "holder"
+    this.outputData = "";
+    const storeLog = inputs => (this.outputData += inputs);
+    console["log"] = jest.fn(storeLog);
   })
 
   describe('Environment Variables', () => {
@@ -12,31 +15,53 @@ describe('Main test', () => {
       process.env.BOARD = "kanban.com"
       process.env.GITHUB_PROFILE = "company-name"
       process.env.PARENT_DIR = "holder"
+      this.outputData = "";
     });
 
     test('it should fail if there is no BOARD env variable', () => {
       process.env.BOARD = ""
       expect(entry.makeEntry()).toBeFalsy();
+      expect(this.outputData).toBe("You're missing the BOARD variable");
     });
     test('it should fail if there is no GITHUB_PROFILE env variable', () => {
       process.env.GITHUB_PROFILE = ""
       expect(entry.makeEntry()).toBeFalsy();
+      expect(this.outputData).toBe("You're missing the GITHUB_PROFILE variable");
     });
     test('it should fail if there is no PARENT_DIR env variable', () => {
       process.env.PARENT_DIR = ""
       expect(entry.makeEntry()).toBeFalsy();
+      expect(this.outputData).toBe("You're missing the PARENT_DIR variable");
     });
   });
 
 
   describe("Handles args correctly", () => {
+    afterEach(() => {
+      this.outputData = "";
+    });
+
     test('it should fail if there are not enough args', () => {
       process.argv = ["", "", "[ticket] stop duplicated course creation #1124"]
       expect(entry.makeEntry()).toBeFalsy();
+      expect(this.outputData).toBe("You have to provide the pull request string and at least one file");
     });
     test('it should return true if successful', () => {
       process.argv = ["", "", "[ticket] stop duplicated course creation #1124", "usr/mike/holder/my-repo/src/thing", "usr/mike/holder/my-repo/src/other"]
+      const date = new Date();
       expect(entry.makeEntry()).toBeTruthy();
+      expect(this.outputData).toBe(`
+ticket https://${process.env.BOARD}/ticket
+Name: stop duplicated course creation
+Date: ${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear() - 2000}
+Description:
+Fix:
+Files:
+\tMY-REPO
+\t/src/thing
+\t/src/other
+Commits: https://github.com/${process.env.GITHUB_PROFILE}/my-repo/pull/1124/commits
+`)
     })
   })
 })
