a5f229b52f3354271d6e588fc6e596f48b3bdcc4
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-10-18 23:31:20 -0400

    make global adjustments

---
+++ bin/demo-global.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+var myLibrary = require('../index.js');
+
+// Displays the text in the console
+myLibrary.makeEntry();
--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-const args = process.argv.slice(2);
+function makeEntry() {const args = process.argv.slice(2);
+  const pullRequestStr = args[0];
+  const files = args.slice(1);
+  const pullRequestTicket = pullRequestStr.match(/^\[(.+)\]/)[1];
+  const pullRequestName = pullRequestStr.match(/\] (.+)\#/)[1];
+  const pullRequestNumber = pullRequestStr.match(/\#(\d+)/)[1];
 
-const pullRequestStr = args[0];
-const files = args.slice(1);
-const pullRequestTicket = pullRequestStr.match(/^\[(.+)\]/)[1];
-const pullRequestName = pullRequestStr.match(/\] (.+)\#/)[1];
-const pullRequestNumber = pullRequestStr.match(/\#(\d+)/)[1];
+  const projectParentFile = files[0].match(process.env.DIR + "/([^\/]+)/")[1];
+  const date = new Date();
 
-const repo = files[0].match(process.env.DIR + "/([^\/]+)/")[1];
-const date = new Date();
+  console.log(`
+  ${pullRequestTicket} ${process.env.BOARD}/${pullRequestTicket}
+  Name: ${pullRequestName}
+  Date: ${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear() - 2000}
+  Description:
+  Fix: 
+  Files:
+  \t${projectParentFile.toUpperCase()}
+  \t${files.map(file => file.split(projectParentFile)[1]).join("\n\t")}
+  Commits: https://github.com/${process.env.GITHUB_PROFILE}/${projectParentFile}/pull/${pullRequestNumber}/commits
+  `)
+}
 
-
-console.log(`
-${pullRequestTicket} ${process.env.BOARD}/${pullRequestTicket}
-Name: ${pullRequestName}
-Date: ${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear() - 2000}
-Description:
-Fix: 
-Files:
-\t${repo.toUpperCase()}
-\t${files.map(file => file.split(repo)[1]).join("\n\t")}
-Commits: https://github.com/${process.env.GITHUB_PROFILE}/${repo}/pull/${pullRequestNumber}/commits
-`)
+exports.makeEntry = makeEntry
