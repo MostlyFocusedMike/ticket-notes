62075169e6ad091d19a74d276acfd7b55d6c7b32
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-10-18 21:32:51 -0400

    get basic args

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const args = process.argv.slice(2);
 
-const pullRequestStr = pull
+const pullRequestStr = args[0];
+const files = args.slice(1);
+const pullRequestTicket = pullRequestStr.match(/^\[(.+)\]/)[1]
+const pullRequestName = pullRequestStr.match(/\] (.+)\#/)[1]
+const pullRequestNumber = pullRequestStr.match(/\#(\d+)/)[1]
+
+const DIR = process.env.DIR
+
+console.log(DIR)
+console.log(pullRequestTicket)
+console.log(pullRequestName)
+console.log(pullRequestNumber)
