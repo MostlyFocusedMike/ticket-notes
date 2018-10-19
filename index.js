978d8ffa7ed80532ec138b1dfcfce83a0888d708
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-10-18 22:10:20 -0400

    get basic entry done

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const args = process.argv.slice(2);
 
 const pullRequestStr = args[0];
 const files = args.slice(1);
-const pullRequestTicket = pullRequestStr.match(/^\[(.+)\]/)[1]
-const pullRequestName = pullRequestStr.match(/\] (.+)\#/)[1]
-const pullRequestNumber = pullRequestStr.match(/\#(\d+)/)[1]
+const pullRequestTicket = pullRequestStr.match(/^\[(.+)\]/)[1];
+const pullRequestName = pullRequestStr.match(/\] (.+)\#/)[1];
+const pullRequestNumber = pullRequestStr.match(/\#(\d+)/)[1];
 
-const repo = files[0].match(process.env.DIR + "/([^\/]+)/")[1]
+const repo = files[0].match(process.env.DIR + "/([^\/]+)/")[1];
 
-console.log(repo)
+const entry = `
+${pullRequestTicket} ${process.env.BOARD}/${pullRequestTicket}
+Name: ${pullRequestName}
+Date:
+Description:
+Fix: 
+Files:
+${repo.toUpperCase()}
+
+Commits: https://github.com${process.env.COMPANY_REPO}/${repo}/pull/${pullRequestNumber}/commits
+`
+
+console.log(entry)
